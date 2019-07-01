5fd82e72d7b72c7c56493c548e9203661aebbfd1
rstorr <reuben@codelingo.io>
2019-07-02 11:32:36 +1200

    init

---
+++ example.go
@@ -0,0 +1,22 @@
+//Package somepackage has a comment
+package somepackage
+
+import (
+    "fmt"
+)
+
+// SomeFunc is an example function
+func SomeFunc() {
+    fmt.Println("A thing")
+}
+
+// SomeFucnc is an example function
+func SomeFucnc() {
+    fmt.Println("A thing")
+}
+
+
+// SomeFunwc is an example function
+func SomeFunwc() {
+    fmt.Println("A thing")
+}
